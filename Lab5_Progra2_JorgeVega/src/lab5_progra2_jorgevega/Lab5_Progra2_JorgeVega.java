e95e099856438df40320f5c9629f6cb7f57c9871
JorgeVega97 <jorgeenriquevegamaradiaga6@gmail.com>
2020-11-20 13:54:16 -0600

    Actualizacion

---
+++ Lab5_Progra2_JorgeVega/src/lab5_progra2_jorgevega/Lab5_Progra2_JorgeVega.java
@@ -0,0 +1,17 @@
+/*
+Lab5 de programacion
+ */
+package lab5_progra2_jorgevega;
+
+/**
+Jorge Vega
+ */
+public class Lab5_Progra2_JorgeVega {
+    public static void main(String[] args) {
+      
+        
+        
+        
+    }
+    
+}
